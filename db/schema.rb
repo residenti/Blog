acbda8d304b6c3ea0335400cf3680a6cd615d654
residenti <n.queue.r@gmail.com>
2019-04-22 00:03:04 +0900

    replace sqlite3 with mysql2

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2019_04_16_094338) do
 
-  create_table "articles", force: :cascade do |t|
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false

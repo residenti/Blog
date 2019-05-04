c18bd61b598f5b19de4c4cc39788254368424719
residenti <n.queue.r@gmail.com>
2019-05-04 21:48:35 +0900

    constantization

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,25 +1,25 @@
 module ArticlesHelper
   require './lib/blog_render.rb'
 
+  OPTIONS = {
+    filter_html: true,
+    hard_wrap: true,
+    space_after_headers: true
+  }
+
+  EXTENSIONS = {
+    autolink: true,
+    fenced_code_blocks: true,
+    lax_spacing: true,
+    no_intra_emphasis: true,
+    strikethrough: true,
+    superscript: true,
+    tables: true
+  }.freeze
+
   def markdown(text)
-    options = {
-      filter_html: true,
-      hard_wrap: true,
-      space_after_headers: true
-    }
-
-    extensions = {
-      autolink: true,
-      fenced_code_blocks: true,
-      lax_spacing: true,
-      no_intra_emphasis: true,
-      strikethrough: true,
-      superscript: true,
-      tables: true
-    }
-
-    renderer = BlogRender.new(options)
-    @markdown = Redcarpet::Markdown.new(renderer, extensions)
+    renderer = BlogRender.new(OPTIONS)
+    @markdown = Redcarpet::Markdown.new(renderer, EXTENSIONS)
     @markdown.render(text).html_safe
   end
 

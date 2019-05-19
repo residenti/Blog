fa5fa414a5e95df31131ff633682461f329fc35d
residenti <n.queue.r@gmail.com>
2019-05-19 21:43:44 +0900

    desc updated_at

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all.order(created_at: "DESC")
+    @articles = Article.all.order(updated_at: "DESC")
   end
 
   # GET /articles/1

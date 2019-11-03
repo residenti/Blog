0fffef874c68a8de5e17155823d6a516533a2858
residenti <n.queue.r@gmail.com>
2019-11-03 13:54:35 +0900

    パスワードリセット削除.

--- db/schema.rb
+++ db/schema.rb
@@ -22,14 +22,11 @@ ActiveRecord::Schema.define(version: 2019_11_02_063426) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
